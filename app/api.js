ed75ea03caf63a6e5f1db2c7a0f014a0e9564702
Nader Dabit <dabit3@gmail.com>
2016-04-06 21:24:45 -0500

    removed api key, not that it matters at this point \_(ツ)_/¯

--- app/api.js
+++ app/api.js
@@ -1,9 +1,8 @@
 var rootUrl = 'https://api.imgur.com/3/gallery/t/'
-var apiKey = '8f67e5f7e9d739a'
+var apiKey = ''
 
 module.exports = {
   get (url) {
-  	console.log('rootUrl + url, ', (rootUrl + url))
     return fetch(rootUrl + url, {
       headers: {
         'Authorization': 'Client-ID ' + apiKey
